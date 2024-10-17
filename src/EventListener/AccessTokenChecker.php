bcfbbf8359208ea7bf78f8916c9a1c6e4017648c
SaHair529 <sa.hairulaev@gmail.com>
2024-10-18 02:28:03 +0300

    Возвращаю ответ с описанием ошибки вместо выбрасывания исключения в AccessTokenChecker.php

--- src/EventListener/AccessTokenChecker.php
+++ src/EventListener/AccessTokenChecker.php
@@ -6,23 +6,28 @@ use App\Entity\AccessToken;
 use DateTimeImmutable;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\RequestEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
 
 class AccessTokenChecker
 {
-    public function __construct(private readonly EntityManagerInterface $entityManager)
+    public function __construct(private readonly EntityManagerInterface $entityManager, private readonly TokenStorageInterface $tokenStorage)
     {
     }
 
     #[AsEventListener(event: KernelEvents::REQUEST)]
-    public function onKernelRequest(RequestEvent $event)
+    public function onKernelRequest(RequestEvent $event): void
     {
+        if (in_array($event->getRequest()->getPathInfo(), ['/api/login', '/api/register'])) {
+            return;
+        }
+
         $request = $event->getRequest();
-//        if (in_array($request->getPathInfo(), ['/api/login', '/api/register'])) { todo uncomment
-//            return;
-//        }
 
         $authorizationHeader = $request->headers->get('Authorization');
 
@@ -30,17 +35,38 @@ class AccessTokenChecker
             $tokenValue = $matches[1];
 
             $accessToken = $this->entityManager->getRepository(AccessToken::class)->findOneBy(['value' => $tokenValue]);
-            if (!$accessToken)
-                throw new AuthenticationException('Invalid access token');
+            if (!$accessToken) {
+                $event->setResponse(new JsonResponse([
+                    'status' => 'error',
+                    'message' => 'Invalid access token',
+                ], Response::HTTP_UNAUTHORIZED));
+                return;
+            }
 
-            if ($accessToken->isRevoked())
-                throw new AuthenticationException('Access token revoked');
+            if ($accessToken->isRevoked()) {
+                $event->setResponse(new JsonResponse([
+                    'status' => 'error',
+                    'message' => 'Access token is revoked',
+                ], Response::HTTP_UNAUTHORIZED));
+                return;
+            }
 
-            if ((new DateTimeImmutable()) > $accessToken->getExpiresAt())
-                throw new AuthenticationException('Access token expired');
+            if ((new DateTimeImmutable()) > $accessToken->getExpiresAt()) {
+                $event->setResponse(new JsonResponse([
+                    'status' => 'error',
+                    'message' => 'Access token expired',
+                ], Response::HTTP_UNAUTHORIZED));
+            }
+
+            $user = $accessToken->getOwner();
+            $token = new UsernamePasswordToken($user, 'main', $user->getRoles());
+            $this->tokenStorage->setToken($token);
         }
         else {
-            throw new AuthenticationException('Authorization header not found');
+            $event->setResponse(new JsonResponse([
+                'status' => 'error',
+                'message' => 'Access token expired',
+            ], Response::HTTP_UNAUTHORIZED));
         }
     }
 }
