05d750c2d44eb11211d668bf2b0269fefda003cb
SaHair529 <sa.hairulaev@gmail.com>
2024-12-19 09:59:38 +0300

    Добавил route для GroupController::list

--- src/Controller/GroupController.php
+++ src/Controller/GroupController.php
@@ -6,9 +6,13 @@ use App\Entity\Group;
 use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
+#[Route('/api/group')]
 class GroupController extends AbstractController
 {
     private User $user;
@@ -21,7 +25,9 @@ class GroupController extends AbstractController
         $this->user = $tokenStorage->getToken()->getUser();
     }
 
-    public function list()
+    #[IsGranted('ROLE_ADMIN')]
+    #[Route('/', methods: ['GET'])]
+    public function list(): JsonResponse
     {
         $groups = $this->entityManager->getRepository(Group::class)->findAll();
         return $this->json($groups, Response::HTTP_OK, [], ['groups' => ['group_list']]);
