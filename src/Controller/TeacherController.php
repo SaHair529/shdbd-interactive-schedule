6c226e306d269a761581cf63f2404d8fd7a739e4
SaHair529 <sa.hairulaev@gmail.com>
2025-02-27 07:41:20 +0300

    Teacher List

---
+++ src/Controller/TeacherController.php
@@ -0,0 +1,27 @@
+<?php
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Enum\UserRole;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/api/teacher')]
+class TeacherController extends AbstractController
+{
+    public function __construct(private EntityManagerInterface $em)
+    {
+    }
+
+    #[Route('/', methods: ['GET'])]
+    public function list()
+    {
+        $teachers = array_filter($this->em->getRepository(User::class)->findAll(), function ($user) {
+            return in_array(UserRole::ROLE_TEACHER, $user->getRoles());
+        });
+        
+        return $this->json(['teachers' => $teachers], Response::HTTP_OK, [], ['groups' => ['user_compact']]);
+    }
+}
--- src/Enum/UserRole.php
+++ src/Enum/UserRole.php
@@ -6,4 +6,5 @@ enum UserRole: string
 {
     case ROLE_USER = 'ROLE_USER';
     case ROLE_ADMIN = 'ROLE_ADMIN';
+    case ROLE_TEACHER = 'ROLE_TEACHER';
 }
